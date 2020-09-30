5eade3b02d441ee54389cf56b543292e6cb72cd0
Bhuvanesh <bhuvaneshcmv@gmail.com>
2020-09-30 09:20:25 +0530

    Initial Commit

---
+++ Assets/Scripts/CubePrefabScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CubePrefabScript : MonoBehaviour
+{
+    public float height;
+    public MeshRenderer CubeMeshRenderer;
+    private void Awake()
+    {
+        LoadValues();
+    }
+
+    private void LoadValues()
+    {
+        height = gameObject.transform.localScale.y;
+        CubeMeshRenderer = this.gameObject.GetComponent<MeshRenderer>();
+        CubeMeshRenderer.material.color =  Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+}
---
+++ Assets/Scripts/CubesGOScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class CubesGOScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] ArrayOfCubes;
+    [SerializeField]
+    public int numberOfCubesNeeded;
+}
+
